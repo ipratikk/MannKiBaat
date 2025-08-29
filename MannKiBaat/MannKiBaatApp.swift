358889aa66cc9e5e5967c3c87259aafdc5fd3008
Pratik Goel <32486561+ipratikk@users.noreply.github.com>
2025-08-30 01:21:42 +0530

    Initial Commit

---
+++ MannKiBaat/MannKiBaatApp.swift
@@ -0,0 +1,32 @@
+//
+//  MannKiBaatApp.swift
+//  MannKiBaat
+//
+//  Created by Pratik Goel on 30/08/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MannKiBaatApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
